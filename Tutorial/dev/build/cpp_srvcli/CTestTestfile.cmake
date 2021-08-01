c2f7c6594d94c45bedd8a5996b370609dbf62309
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-08-01 12:39:49 -0700

    Created Service/Client in c++ following tutorial

---
+++ Tutorial/dev/build/cpp_srvcli/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/src/cpp_srvcli
+# Build directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/build/cpp_srvcli
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
