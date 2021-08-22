e82317bd7bf91f4b4e02b784749316118a719c28
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-08-22 16:16:15 -0400

    Using parameters in a C++ class

---
+++ Tutorial/dev/build/cpp_parameters/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/src/cpp_parameters
+# Build directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/build/cpp_parameters
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
