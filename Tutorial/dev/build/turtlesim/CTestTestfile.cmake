88a6e18de0694ac29902393f1cfee360cba62688
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-09-01 20:33:59 -0400

    Removed Tutorial folder

--- Tutorial/dev/build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/src/ros_tutorials/turtlesim
-# Build directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
--- Tutorial/dev/build/tutorial_interfaces/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/src/tutorial_interfaces
-# Build directory: /home/adriancooper/Desktop/SeniorDesign/Tutorial/dev/build/tutorial_interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tutorial_interfaces__py")
