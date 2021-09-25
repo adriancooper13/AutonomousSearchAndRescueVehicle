619e8a93a0c8656311af1c970a7c30efec139a31
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-09-25 17:33:27 -0400

    rearranged files

--- dev/src/helpers/setup.py
+++ dev/src/helpers/setup.py
@@ -5,6 +5,7 @@ package_name = 'helpers'
 setup(
     name=package_name,
     version='0.0.0',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
