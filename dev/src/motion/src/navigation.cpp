753628f27df1d883cfeff075bc219bed601a6e27
Adrian Cooper <adriancooperwrx13@gmail.com>
2021-11-07 15:48:51 -0500

    tweaked threshold and added joint state sub

--- dev/src/motion/src/navigation.cpp
+++ dev/src/motion/src/navigation.cpp
@@ -1,8 +1,9 @@
+#include "diff_drive_controller/diff_drive_controller.hpp"
 #include "geometry_msgs/msg/point.hpp"
 #include "geometry_msgs/msg/pose.hpp"
-#include "../../helpers.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/int32.hpp"
 
@@ -10,6 +11,50 @@
 #define ANGULAR_VELOCITY_FACTOR     -0.01
 #define MAX_SPEED                   0.91
 #define FIELD_SIZE                  22.86
+#define WHEEL_SEPARATION            0.287
+#define WHEEL_RADIUS                0.033
+
+std::map<char, double> euler_from_quaternion(geometry_msgs::msg::Quaternion orientation)
+{
+    auto rpy = std::map<char, double>();
+
+    // roll (x-axis rotation)
+    double sinr_cosp = 2 * (orientation.w * orientation.x + orientation.y * orientation.z);
+    double cosr_cosp = 1 - 2 * (orientation.x * orientation.x + orientation.y * orientation.y);
+    rpy['x'] = std::atan2(sinr_cosp, cosr_cosp);
+
+    // pitch (y-axis rotation)
+    double sinp = 2 * (orientation.w * orientation.y - orientation.z * orientation.x);
+    if (std::abs(sinp) >= 1)
+        rpy['y'] = std::copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+    else
+        rpy['y'] = std::asin(sinp);
+
+    // yaw (z-axis rotation)
+    double siny_cosp = 2 * (orientation.w * orientation.z + orientation.x * orientation.y);
+    double cosy_cosp = 1 - 2 * (orientation.y * orientation.y + orientation.z * orientation.z);
+    rpy['z'] = std::atan2(siny_cosp, cosy_cosp);
+
+    return rpy;
+}
+
+geometry_msgs::msg::Quaternion quaternion_from_euler(double roll, double pitch, double yaw)
+{
+    double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+
+    auto q = geometry_msgs::msg::Quaternion();
+    q.x = cy * cp * sr - sy * sp * cr;
+    q.y = sy * cp * sr + cy * sp * cr;
+    q.z = sy * cp * cr - cy * sp * sr;
+    q.w = cy * cp * cr + sy * sp * sr;
+
+    return q;
+}
 
 class Navigation : public rclcpp::Node
 {
@@ -19,10 +64,12 @@ class Navigation : public rclcpp::Node
         rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr stop_subscriber;
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber;
         rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr manual_control_subscriber;
+        rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_subscriber;
         // Current pose for detecting edges.
         geometry_msgs::msg::Pose pose;
         // Determines if manual control is enabled / if we need to stop.
         bool manual_control, stop;
+
 
         enum TurnDirection {
             LEFT = 1,
@@ -40,7 +87,7 @@ class Navigation : public rclcpp::Node
                 "cmd_vel",
                 10
             );
-
+            
             ball_direction_subscriber = create_subscription<std_msgs::msg::Int32>(
                 "direction",
                 5,
@@ -88,7 +135,7 @@ class Navigation : public rclcpp::Node
 
         TurnDirection determine_direction()
         {
-            const double THRESHOLD = (FIELD_SIZE / 2) - 1.5;
+            const double THRESHOLD = (FIELD_SIZE / 2) - 0.75;
             
             double theta = euler_from_quaternion(pose.orientation)['z'];
             if (pose.position.x > THRESHOLD)
