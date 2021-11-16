f19008c97279cf675d9b60bb9ec5b1e9a29b1711
adriancooper13 <adriancooperwrx13@gmail.com>
2021-11-16 04:45:07 +0000

    fixed pwm calculation and added sleep(5) to get consistent results with arduino

--- dev/src/motion/src/arduino_controller.py
+++ dev/src/motion/src/arduino_controller.py
@@ -5,6 +5,7 @@ import serial
 from geometry_msgs.msg import Twist
 from math import pi as PI
 from rclpy.node import Node
+from time import sleep
 
 ABS_MAX_PWM = 255
 MAX_PWM = 153
@@ -15,6 +16,7 @@ class ArduinoController(Node):
     def __init__(self):
         super().__init__('arduino_controller')
         self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+        sleep(5)
         self.ser.flush()
 
         self.cmd_vel_subscriber = self.create_subscription(
@@ -26,21 +28,26 @@ class ArduinoController(Node):
         
         self.get_logger().info(f'{self.get_name()} node has started')
 
-    def send_velocity(self, msg: Twist):
+    def send_velocity(self, msg: Twist):        
         left, right = self.pwm(msg.linear.x, msg.angular.z)
 
         string = f'{left} {right}' + '\n'
         sent = self.ser.write(string.encode('utf-8'))
         
-        line = self.ser.readline().decode('utf-8').rstrip('\n')
-        self.get_logger().info(f'{line}')
+        line = self.ser.readline().decode('utf-8').rstrip()
         
-    def pwm(self, linear, angular):
-        total_pwm = int(linear * MAX_PWM / MAX_MSEC) if linear != 0 else 1
-        x = int(angular * total_pwm / PI)
+        self.get_logger().info(f'Bytes Sent: {sent}. Received: {line}')
         
-        left_pwm = total_pwm - x
-        right_pwm = total_pwm + x
+    def pwm(self, linear: float, angular: float):
+        if linear != 0.0:
+            total_pwm = linear * MAX_PWM / MAX_MSEC
+            x = angular * total_pwm / PI
+        else:
+            total_pwm = 0
+            x = MAX_PWM * angular / PI
+        
+        left_pwm = round(total_pwm - x)
+        right_pwm = round(total_pwm + x)
         
         if right_pwm > ABS_MAX_PWM:
             diff = right_pwm - ABS_MAX_PWM
